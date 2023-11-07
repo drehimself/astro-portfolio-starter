be7c7d669ad60edb3678e8855543edd93dfb1bcb
Andre Madarang <andre_madarang@hotmail.com>
2023-11-07 15:42:38 -0500

    Remove Vue component, just use vanilla JS to minimize FOUC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind()],
 })
