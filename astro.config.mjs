69fd4bb95d33ac341e482347a8c91d5d9a891481
Andre Madarang <andre_madarang@hotmail.com>
2023-11-13 14:30:29 -0500

    Change syntax highlighting theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue()]
-});
+  integrations: [tailwind(), mdx(), react(), vue()],
+  markdown: {
+    shikiConfig: {
+      theme: 'material-theme',
+    },
+  },
+})
